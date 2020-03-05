2022c3c2343eb0cef921617b9337a4811d9e1e99
Josenaldo <josenaldo.asj@gmail.com>
2020-03-05 11:48:57 -0300

    Create auxiliary function to decrease code repetition

--- proj01/src/parser/parser.cpp
+++ proj01/src/parser/parser.cpp
@@ -11,6 +11,25 @@ using std::make_unique;
 using std::string;
 using std::stringstream;
 
+/* Since there are no template variables, this piece of code is here in the .cpp file */
+/* To avoid the "specialization after initiation" error, the implementation of the function
+should be done before it is actually used */
+template <>
+void addItemToParamset<unsigned char>(const TiXmlElement* el, const char* tag, int size, string param, ParamSet& ps){
+	string val;
+
+	if(el->QueryStringAttribute(tag, &val) == TIXML_SUCCESS ){
+		stringstream stream(val);
+		auto arr = make_unique<unsigned char[]>(size);
+		short tmp;
+		for(int i = 0; i < size; ++i){
+			stream >> tmp;
+			arr[i] = (unsigned char)tmp;
+		}
+		ps.add<unsigned char>(param, move(arr), size);
+	}
+}
+
 Rt3 parse(const string input){
 	Rt3 rt3Obj;
 
@@ -53,118 +72,26 @@ unique_ptr<Scene> parseScene(const TiXmlElement* world){
 
 unique_ptr<Background> parseBackground(const TiXmlElement* background){
 	ParamSet psBack;
-	string type, mapping, bl, br, tl, tr, color;
-	short r,g,b;
 
-	if(background->QueryStringAttribute("type", &type) == TIXML_SUCCESS){
-		auto typeArr = make_unique<string[]>(1);
-		typeArr[0] = type;
-		psBack.add<string>("type", move(typeArr));
-	}
-
-	if(background->QueryStringAttribute("mapping", &mapping) == TIXML_SUCCESS){
-		auto mappingArr = make_unique<string[]>(1);
-		mappingArr[0] = mapping;
-		psBack.add<string>("mapping", move(mappingArr));
-	}
-
-	if(background->QueryStringAttribute("bl", &bl) == TIXML_SUCCESS){
-		stringstream blStream(bl);
-		blStream >> r >> g >> b;
-
-		auto blArr = make_unique<unsigned char[]>(3);
-		blArr[0] = (unsigned char)r;
-		blArr[1] = (unsigned char)g;
-		blArr[2] = (unsigned char)b;
-		
-		psBack.add<unsigned char>("bl", move(blArr), 3);
-	}	
-
-	if(background->QueryStringAttribute("br", &br) == TIXML_SUCCESS){
-		stringstream brStream(br);
-		brStream >> r >> g >> b;
-
-		auto brArr = make_unique<unsigned char[]>(3);
-		brArr[0] = (unsigned char)r;
-		brArr[1] = (unsigned char)g;
-		brArr[2] = (unsigned char)b;
-		
-		psBack.add<unsigned char>("br", move(brArr), 3);
-	}
-
-	if(background->QueryStringAttribute("tl", &tl) == TIXML_SUCCESS){
-		stringstream tlStream(tl);
-		tlStream >> r >> g >> b;
-
-		auto tlArr = make_unique<unsigned char[]>(3);
-		tlArr[0] = (unsigned char)r;
-		tlArr[1] = (unsigned char)g;
-		tlArr[2] = (unsigned char)b;
-		
-		psBack.add<unsigned char>("tl", move(tlArr), 3);
-	}
-
-	if(background->QueryStringAttribute("tr", &tr) == TIXML_SUCCESS){
-		stringstream trStream(tr);
-		trStream >> r >> g >> b;
-
-		auto trArr = make_unique<unsigned char[]>(3);
-		trArr[0] = (unsigned char)r;
-		trArr[1] = (unsigned char)g;
-		trArr[2] = (unsigned char)b;
-		
-		psBack.add<unsigned char>("tr", move(trArr), 3);
-	}
-
-	if(background->QueryStringAttribute("color", &color) == TIXML_SUCCESS){
-		stringstream colorStream(color);
-		colorStream >> r >> g >> b;
-
-		auto colorArr = make_unique<unsigned char[]>(3);
-		colorArr[0] = (unsigned char)r;
-		colorArr[1] = (unsigned char)g;
-		colorArr[2] = (unsigned char)b;
-		
-		psBack.add<unsigned char>("color", move(colorArr), 3);
-	}
+	addItemToParamset<string>(background, "type", 1, "type", psBack);
+	addItemToParamset<string>(background, "mapping", 1, "mapping", psBack);
+	addItemToParamset<unsigned char>(background, "bl", 3, "bl", psBack);
+	addItemToParamset<unsigned char>(background, "br", 3, "br", psBack);
+	addItemToParamset<unsigned char>(background, "tl", 3, "tl", psBack);
+	addItemToParamset<unsigned char>(background, "tr", 3, "tr", psBack);
+	addItemToParamset<unsigned char>(background, "color", 3, "color", psBack);
 
 	return make_unique<Background>(psBack);
 }
 
 unique_ptr<Film> parseFilm(const TiXmlElement* film){
 	ParamSet psFilm;
-	int xRes, yRes;
-	string outFileName, imgType, type;
-
-	if(film->QueryIntAttribute("x_res", &xRes) == TIXML_SUCCESS){
-		auto xResArr = make_unique<int[]>(1);
-		xResArr[0] = xRes;
-		psFilm.add<int>("xRes", move(xResArr));
-	}
-
-	if(film->QueryIntAttribute("y_res", &yRes) == TIXML_SUCCESS){
-		auto yResArr = make_unique<int[]>(1);
-		yResArr[0] = yRes;
-		psFilm.add<int>("yRes", move(yResArr));
-	}
-
-	if(film->QueryStringAttribute("filename", &outFileName) == TIXML_SUCCESS){
-		auto filenameArr = make_unique<string[]>(1);
-		filenameArr[0] = outFileName;
-		psFilm.add<string>("filename", move(filenameArr));
-	}
-
-	if(film->QueryStringAttribute("img_type", &imgType) == TIXML_SUCCESS){
-		auto imgTypeArr = make_unique<string[]>(1);
-		imgTypeArr[0] = imgType;
-		psFilm.add<string>("imgType", move(imgTypeArr));
-	}
-
-	if(film->QueryStringAttribute("type", &type) == TIXML_SUCCESS){
-		auto typeArr = make_unique<string[]>(1);
-		typeArr[0] = imgType;
-		psFilm.add<string>("type", move(typeArr));
-	}
+	
+	addItemToParamset<int>(film, "x_res", 1, "xRes", psFilm);
+	addItemToParamset<int>(film, "y_res", 1, "yRes", psFilm);
+	addItemToParamset<string>(film, "filename", 1, "filename", psFilm);
+	addItemToParamset<string>(film, "img_type", 1, "imgType", psFilm);
+	addItemToParamset<string>(film, "type", 1, "type", psFilm);
 
 	return make_unique<Film>(psFilm);
 }
@@ -172,11 +99,8 @@ unique_ptr<Film> parseFilm(const TiXmlElement* film){
 unique_ptr<Camera> parseCamera(const TiXmlElement* camera){
 	ParamSet ps;
 
-	string type;
-	if(camera->QueryStringAttribute("type", &type) == TIXML_SUCCESS){
-		auto arr_type = std::make_unique<string[]>(1);
-		ps.add<string>("type", move(arr_type));
-	}
+	addItemToParamset<string>(camera, "type", 1, "type", ps);
 
 	return make_unique<Camera>(ps);
 }
+
